8b33de926feddedef93780ccd29937afaa4ba15d
Margarita Kachalko <margarita.kachalko@gmail.com>
2021-12-02 17:33:59 +0300

    lesson06 additional part

--- script.js
+++ script.js
@@ -5,27 +5,41 @@ const isNumber = function (value) {
   return !isNaN(parseFloat(value)) && isFinite(value);
 };
 
+
 function getNumber () {
   let n = Math.floor(Math.random() * 100 + 1);
-  
+  let i = 10;
 
   function testNumber () {
-    let userNam = prompt("Угадай число от 1 до 100");
+    let userNam = prompt("Угадай число от 1 до 100. У тебя осталось" + " " + i + " " + "попыток");
+    i--;
 
-    if (userNam == null) {
-    alert("Игра окончена");
-  } else if (userNam > n) {
-    alert("Загаданное число меньше");
-    testNumber();
-  }  else if (userNam < n) {
-    alert("Загаданное число больше");
-    testNumber();
-  } else if (!isNumber(userNam)) {
-    alert("Введи число!");
-    testNumber();
-  } else if (userNam == n) {
-    alert ("Поздравляю, Вы угадали!!!");
-  }
+    if (i<=0) {
+      let end = confirm("Попытки закончились, хотите сыграть еще?");
+      if (end) {
+        getNumber();
+      }
+
+    } else if (userNam == null) {
+      alert("Игра окончена");
+    } else if (!isNumber(userNam)) {
+      alert("Введи число!");
+      i++;
+      testNumber();
+    } else if (userNam > n) {
+      alert("Загаданное число меньше");
+      testNumber();
+    }  else if (userNam < n) {
+      alert("Загаданное число больше");
+      testNumber();
+    } else if (userNam == n) {
+      let bingo = confirm ("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?");
+      if (bingo) {
+        getNumber ();
+      }
+    }
+
+    
   }
 
   testNumber();
