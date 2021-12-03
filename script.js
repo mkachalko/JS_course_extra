ede9b3da92f13a6cbb64e7ded359b8337f059e79
Margarita Kachalko <margarita.kachalko@gmail.com>
2021-12-03 22:45:27 +0300

    lesson07 completed

---
+++ script.js
@@ -0,0 +1,45 @@
+"use strict";
+
+let week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+let todayDate = new Date();
+let todayIndex = todayDate.getDay();
+
+const body = document.querySelector('body');
+
+
+const getList = function (array, today) {
+
+  let result = [];
+    
+  for (let i = 0; i < array.length; i++) {
+    
+    let li = document.createElement("li");
+    
+    if (i == today) {
+      li.append(array[i]);
+      li.style.fontWeight = "bold";
+      result.push(li);
+      
+    } else if(array[i] == "Sunday" || array[i] == "Saturday" ) {
+      li.append(array[i]);
+      li.style.fontStyle = "italic";
+      result.push(li);
+    } else {
+      li.append(array[i]);
+      result.push(li);
+    }
+    
+  }
+
+  return result;
+};
+
+
+body.prepend(document.createElement("ul"));
+document.querySelector('ul').append(...getList(week, todayIndex));
+
+
+
+
+
